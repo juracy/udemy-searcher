3d6f5fa19711360c4d1f9a5ccee03907af8bfc13
Juracy Filho <juracy@gmail.com>
2019-03-21 14:27:56 -0300

    Replace API (instructor) for discussion (user)- Getting URL

--- udemy-searcher.py
+++ udemy-searcher.py
@@ -5,7 +5,9 @@ import sys
 
 import requests
 
-ENDPOINT = 'https://www.udemy.com/instructor-api/v1/'
+INSTRUCTOR_ENDPOINT = 'https://www.udemy.com/instructor-api/v1/'
+USER_ENDPOINT = 'https://www.udemy.com/api-2.0/users/me/'
+UDEMY_QUESTIONS = 'https://www.udemy.com/{}/learn/v4/questions/'
 
 
 def query_string(params):
@@ -20,40 +22,19 @@ def match(terms, text):
     return terms.lower() in text.lower()
 
 
-class Question:
-    def __init__(self, parent, id, title):
+class Discussion:
+    def __init__(self, parent, id, title, body, course):
         self.parent = parent
         self.id = id
         self.title = title
+        self.body = body
+        self.course = course.replace('/', '')
         self.replies = []
 
-
-class Course:
-    def __init__(self, parent, id, title):
-        self.parent = parent
-        self.id = id
-        self.title = title
-        self.questions = []
-
-    def get_questions(self):
-        params = param_fields({
-            'question': ['title', 'num_replies', 'replies', 'content'],
-            'answer': ['body'],
-        })
-        params['page_size'] = 100
-        url = f'{ENDPOINT}courses/{self.id}/questions/?{query_string(params)}'
-        while True:
-            response = requests.get(url, headers=self.parent.headers)
-            data = response.json()
-            for question in data['results']:
-                q = Question(self, question['id'], question['title'])
-                self.questions.append(q)
-                for answer in question['replies']:
-                    q.replies.append(answer['body'])
-            if data['next'] is None:
-                break
-
-            url = data['next']
+    def match(self, terms):
+        return match(terms, self.title) or \
+               match(terms, self.body) or \
+               any(match(terms, r) for r in self.replies)
 
 
 class Udemy:
@@ -67,32 +48,35 @@ class Udemy:
         }
 
         self.courses = []
+        self.discussions = []
 
-    def load(self):
-        self.get_courses()
+    def get_discussions(self):
+        params = param_fields({
+            'course_discussion': ['id', 'title', 'body', 'replies', 'course'],
+            'course_discussion_reply': ['body'],
+            'course': ['url'],
+        })
+        params['page_size'] = 100
+        url = f'{USER_ENDPOINT}searchable-taught-courses-discussions/?{query_string(params)}'
+        while True:
+            response = requests.get(url, headers=self.headers)
+            data = response.json()
+            for discussion in data['results']:
+                q = Discussion(
+                    self,
+                    discussion['id'],
+                    discussion['title'],
+                    discussion['body'],
+                    discussion['course']['url'])
+                q.replies = [reply['body'] for reply in discussion['replies']]
+                self.discussions.append(q)
+            if data['next'] is None:
+                break
 
-        for course in u.courses:
-            course.get_questions()
-
-    def get_courses(self):
-        url = f'{ENDPOINT}taught-courses/courses/?fields[course]=title,is_paid'
-        response = requests.get(url, headers=self.headers)
-        for course in response.json()['results']:
-            if course['is_paid']:
-                self.courses.append(Course(self, course['id'], course['title']))
+            url = data['next']
 
     def search(self, terms):
-        list = []
-        for c in self.courses:
-            for q in c.questions:
-                if match(terms, q.title):
-                    list.append(q)
-                else:
-                    for r in q.replies:
-                        if match(terms, r):
-                            list.append(q)
-                            break
-        return list
+        return [q for q in self.discussions if q.match(terms)]
 
 
 if __name__ == '__main__':
@@ -103,7 +87,8 @@ if __name__ == '__main__':
         sys.exit(1)
 
     print('loading...')
-    u.load()
+    u.get_discussions()
+    print(f'{len(u.discussions)} discussions loaded')
 
     while True:
         terms = input('Terms: ')
@@ -114,6 +99,9 @@ if __name__ == '__main__':
         if results:
             for result in results:
                 print(f'******* {result.title}')
+                print(f'URL: {UDEMY_QUESTIONS.format(result.course)}{result.id}/')
+                print(result.body)
+                print('----')
                 print('\n'.join(result.replies))
                 print('**\n')
         else:
